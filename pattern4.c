5f3f1d3073dd6df3473f5a2464016aeaf09c7c96
Yashwant077 <84570757+Yashwant077@users.noreply.github.com>
2021-10-01 10:08:00 +0530

    Create pattern4.c

---
+++ pattern4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+    char alpha;
+    printf("Enter an alphabet: ");
+    scanf("%c", &alpha);
+    for(i='A';i<=alpha;i++)
+    {
+        for(j='A';j<=i;j++)
+        {
+            printf("%c ",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+Output::
+Enter an alphabet: F
+A 
+B B 
+C C C 
+D D D D 
+E E E E E 
+F F F F F F 
+*/
